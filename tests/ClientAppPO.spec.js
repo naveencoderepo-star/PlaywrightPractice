a96d89c2f20865305b67ed1828a585a8d7ff70ee
naveencoderepo-star <naveencoderepo@gmail.com>
2026-02-03 18:01:10 +0530

    fix

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,36 +1,41 @@
 const { test } = require('@playwright/test')
+
 const { LoginPage } = require('../pageobject/LoginPage')
 const { DashboardPage } = require('../pageobject/DashboardPage')
 const { CartPage } = require('../pageobject/CartPage')
 const { CheckoutPage } = require('../pageobject/CheckoutPage')
 const { OrdersPage } = require('../pageobject/OrdersPage')
 
-test('@Webst Client App – place order flow', async ({ page }) => {
-  const email = 'anshika@gmail.com'
-  const password = 'Iamking@000'
-  const productName = 'ZARA COAT 3'
+const dataset = require('../utlis/placeOrderTestData.json')
 
-  const loginPage = new LoginPage(page)
-  const dashboardPage = new DashboardPage(page)
-  const cartPage = new CartPage(page)
-  const checkoutPage = new CheckoutPage(page)
-  const ordersPage = new OrdersPage(page)
+test.describe('Place Order – Data Driven', () => {
+  for (const data of dataset) {
 
-  await loginPage.goTo()
-  await loginPage.validLogin(email, password)
+    test(`@Webst Client App ${data.productName}`, async ({ page }) => {
 
-  await dashboardPage.addProductToCart(productName)
-  await dashboardPage.goToCart()
+      const loginPage = new LoginPage(page)
+      const dashboardPage = new DashboardPage(page)
+      const cartPage = new CartPage(page)
+      const checkoutPage = new CheckoutPage(page)
+      const ordersPage = new OrdersPage(page)
 
-  await cartPage.verifyProductInCart(productName)
-  await cartPage.checkout()
+      await loginPage.goTo()
+      await loginPage.validLogin(data.email, data.password)
 
-  await checkoutPage.selectCountry('ind')
-  await checkoutPage.verifyEmail(email)
-  const orderId = await checkoutPage.placeOrder()
+      await dashboardPage.addProductToCart(data.productName)
+      await dashboardPage.goToCart()
 
-  console.log('Order ID:', orderId)
+      await cartPage.verifyProductInCart(data.productName)
+      await cartPage.checkout()
 
-  await ordersPage.openOrder(orderId)
-  await ordersPage.verifyOrderDetails(orderId)
+      await checkoutPage.selectCountry('ind')
+      await checkoutPage.verifyEmail(data.email)
+
+      const orderId = await checkoutPage.placeOrder()
+      console.log('Order ID:', orderId)
+
+      await ordersPage.openOrder(orderId)
+      await ordersPage.verifyOrderDetails(orderId)
+    })
+  }
 })
